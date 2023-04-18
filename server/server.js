74a798a471682d9383e0e3be86caf02f12a91c72
parasjogani <paras.jogani@gmail.com>
2023-04-18 09:33:18 +0530

    set up socket io server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 import mongoose from "mongoose"
 import app from "./app.js"
 import config from "./config/index.js"
+import http from "http"
+import { Server } from "socket.io"
 
 (async () => {
     try {
         await mongoose.connect(config.MONGODB_URI)
         console.log("DB Connected Successfully");
 
-        app.on("error", (err) => {
-            console.log(err);
-            throw err;
+        // Create HTTP server and Socket.io instance
+        const server = http.createServer(app)
+        const io = new Server(server)
+
+        // Socket.io connection listener
+        io.on('connection', (socket)=>{
+            console.log('A user has connected');
+
+            // Socket.io disconnection listener
+            socket.on('disconnect', ()=>{
+                console.log('A user has disconnected');
+            })
+
+            // Socket.io message listener
+            socket.on('chat message', (msg)=>{
+                console.log(`Message: ${msg}`);
+                io.emit('chat message', msg)
+            })
         })
 
-        app.listen(config.PORT, () => {
-            console.log(`Port listening at ${config.PORT}`);
+        server.listen(config.PORT, () => {
+            console.log(`Server listening on port ${config.PORT}`);
         })
     } catch (error) {
         console.log(error);
